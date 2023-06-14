4cd74eece79e05f281a8b30189ba7f447d4c5d94
KuznetsovKU <kuznetsov_k@outlook.com>
2023-06-14 21:50:21 +0300

    added some experiments

--- scripts/chat.js
+++ scripts/chat.js
@@ -11,7 +11,16 @@ function post_message(message){
 function lets_chat(){
     // post_message(get_message);
     chat_message = document.getElementById("chat_message_text").value;
-    console.log(chat_message)
+    console.log(chat_message);
     document.getElementById("new_message").innerText = chat_message;
     document.getElementById("chat_message_text").value = '';
 }
+
+function changeChat() {
+    let chat = document.getElementsByClassName('chat_content');
+    chat[0].classList.toggle('joke')
+
+}
+
+let send_message_button = document.getElementById("Send_message_button");
+send_message_button.addEventListener('click', changeChat);
--- scripts/greetings.js
+++ scripts/greetings.js
@@ -3,4 +3,21 @@ function greetings(){
     userName = document.getElementById("name").value;
     alert(`Hi ${userName} Sorry, but you can't join us right now. It still in progress.`);
     document.getElementById("name").value = '';
+    document.getElementById("email").value = '';
+    document.getElementById("password").value = '';
 }
+
+function paintButton() {
+    let buttons = document.getElementsByClassName("join_btn");
+    for (let index = 0; index < buttons.length; index++) {
+        buttons[index].classList.toggle("active_button");
+    }
+    greetings();
+}
+
+let enter_button = document.getElementById("Enter_button");
+enter_button.addEventListener('click', paintButton)
+
+
+let register_button = document.getElementById("Register_button");
+register_button.addEventListener('click', paintButton)
